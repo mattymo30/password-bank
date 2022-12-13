abe5f77e07c55e58086057b6632eb1e8922ddc37
mattymo30 <120498834+mattymo30@users.noreply.github.com>
2022-12-13 13:12:36 -0500

    Basic Login/Register Page
    First attempts at a very basic login/register page with tkinter.

--- passwordGenerator/login.py
+++ passwordGenerator/login.py
@@ -7,7 +7,7 @@ def main_screen_login():
     login_screen.geometry("400x300")
     Label(text="Password Bank", font=25).pack()
     Label(text="").pack()
-    Button(text="Login", height="2", width="30").pack()
+    Button(text="Login", height="2", width="30", command=login).pack()
     Label(text="").pack()
     Button(text="Register", height="2", width="30", command=register).pack()
     login_screen.mainloop()
@@ -17,11 +17,24 @@ def register():
     register_screen = Tk()
     register_screen.title("Registration")
     register_screen.geometry("400x300")
+    Label(register_screen, text="Register User", font=25).pack()
     Label(register_screen, text="Username").pack()
     Entry(register_screen).pack()
     Label(register_screen, text="Password").pack()
     Entry(register_screen).pack()
     register_screen.mainloop()
+
+
+def login():
+    login_screen = Tk()
+    login_screen.title("Login")
+    login_screen.geometry("400x300")
+    Label(login_screen, text="Login", font=25).pack()
+    Label(login_screen, text="Username").pack()
+    Entry(login_screen).pack()
+    Label(login_screen, text="Password").pack()
+    Entry(login_screen).pack()
+    login_screen.mainloop()
 
 
 def main():
