a70e6398715d0838b3022ab52832b274407a6065
mattymo30 <120498834+mattymo30@users.noreply.github.com>
2022-12-13 20:58:41 -0500

    Encrypt
    Properly add to txt file the username, the encrypted password, and the key to the txt files

--- password_application/register.py
+++ password_application/register.py
@@ -1,14 +1,15 @@
 from cryptography.fernet import Fernet
 
 
-key = Fernet.generate_key()
-f = Fernet(key)
-
-
 def registration(username, password):
-    file = open("database.txt", 'wt')
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    file = open("database.txt", 'a')
     byte_pass = bytes(password, 'utf-8')
     encrypt_pass = f.encrypt(byte_pass)
-    file.write(str(username))
+    file.write(str(username) + '\t')
     file.write(str(encrypt_pass))
+    file.write('\n')
     file.close()
+    key_file = open("keys.txt", 'a')
+    key_file.write(str(key))
